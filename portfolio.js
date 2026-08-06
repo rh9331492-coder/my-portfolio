37b3dffaaae51d140312df918178d11f8e57906a
rh9331492-coder <rh9331492@gmail.com>
2026-08-06 19:36:19 +0600

    animation cursor added

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,5 @@
-function toggleMenu() {
-    document.getElementById("navMenu").classList.toggle("active");
-}
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener("mousemove", (e) => {
+    cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px) translate(-50%, -50%)`;
+});
