9cf4b4c75e1e6c708f161b50d68ebded8d8f94be
rh9331492-coder <rh9331492@gmail.com>
2026-08-24 20:11:16 +0600

    section added now

--- portfolio.js
+++ portfolio.js
@@ -16,3 +16,27 @@ const cursor = document.querySelector('.cursor');
 document.addEventListener("mousemove", (e) => {
     cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px) translate(-50%, -50%)`;
 });
+
+
+
+// slider js
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".project-card").forEach((card, index) => {
+
+  gsap.to(card, {
+    opacity: 1,
+    y: 0,
+
+    duration: 1.2,
+    ease: "power4.out",
+
+    scrollTrigger: {
+      trigger: card,
+      start: "top 85%",
+      end: "top 45%",
+      scrub: 1
+    }
+  });
+
+});
